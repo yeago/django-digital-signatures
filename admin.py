90b786fdbf0ec102ab7f06d81816061c2c6133a7
Stephen <subsume@gmail.com>
2010-02-16 19:02:30 +0000

    ...and forgot to add it. added.

---
+++ admin.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib import admin
+from django.conf import settings
+from django_digital_signature import models as smodels
+
+class SignatoryAdminForm(forms.ModelForm):
+	def __init__(self,*args,**kwargs):
+		super(SignatoryAdminForm,self).__init__(*args,**kwargs)
+		if hasattr(settings,'DIGITAL_SIGNATURE_AUTH_TOKEN_NAME'):
+			self.fields['auth_token'].label = settings.DIGITAL_SIGNATURE_AUTH_TOKEN_NAME
+
+	class Meta:
+		model = smodels.Document
+
+class DocumentAdmin(admin.ModelAdmin):
+	list_display = ('title','author','content_object','date_created')
+
+class SignatoryAdmin(admin.ModelAdmin):
+	list_display = ('document','user','date_signed')
+	form = SignatoryAdminForm
+
+admin.site.register(smodels.Signatory,SignatoryAdmin)
+admin.site.register(smodels.Document,DocumentAdmin)
