23f89b0efc166f3350329c8ee3a6079464fa7da8
Stephen <subsume@gmail.com>
2010-05-24 18:16:52 +0000

    updated to 1.2 messaging

--- views.py
+++ views.py
@@ -2,11 +2,14 @@ from django_digital_signature.forms import SignatureForm
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 
+from django.messages import messages
+
 def apply_signature(request,return_url="/",success_msg="Digital signature has been applied"):
 	form = SignatureForm(request.POST or None,user=request.user)
 	if form.is_valid():
 		form.save()
-		request.user.message_set.create(message=success_msg)
+		messages.success(request,success_msg)
 	else:
-		request.user.message_set.create(message="Signature not applied.")
+		messages.error(request,"Signature not applied.")
+		
 	return redirect(request.GET.get("return_url","/"))
