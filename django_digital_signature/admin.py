165f2d8ab2120938fcb5b9a68a3679e4bbf460d4
=yeago <yeago999@gmail.com>
2016-09-06 12:38:09 -0700

    more 1.8 work

--- django_digital_signature/admin.py
+++ django_digital_signature/admin.py
@@ -3,21 +3,25 @@ from django.contrib import admin
 from django.conf import settings
 from django_digital_signature import models as smodels
 
-class SignatoryAdminForm(forms.ModelForm):
-	def __init__(self,*args,**kwargs):
-		super(SignatoryAdminForm,self).__init__(*args,**kwargs)
-		if hasattr(settings,'DIGITAL_SIGNATURE_AUTH_TOKEN_NAME'):
-			self.fields['auth_token'].label = settings.DIGITAL_SIGNATURE_AUTH_TOKEN_NAME
 
-	class Meta:
-		model = smodels.Document
+class SignatoryAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SignatoryAdminForm, self).__init__(*args, **kwargs)
+        if hasattr(settings, 'DIGITAL_SIGNATURE_AUTH_TOKEN_NAME'):
+            self.fields['auth_token'].label = settings.DIGITAL_SIGNATURE_AUTH_TOKEN_NAME
+
+    class Meta:
+        model = smodels.Document
+        fields = '__all__'
+
 
 class DocumentAdmin(admin.ModelAdmin):
-	list_display = ('title','author','content_object','date_created')
+    list_display = ('title', 'author', 'content_object', 'date_created')
+
 
 class SignatoryAdmin(admin.ModelAdmin):
-	list_display = ('document','user','date_signed')
-	form = SignatoryAdminForm
+    list_display = ('document', 'user', 'date_signed')
+    form = SignatoryAdminForm
 
-admin.site.register(smodels.Signatory,SignatoryAdmin)
-admin.site.register(smodels.Document,DocumentAdmin)
+admin.site.register(smodels.Signatory, SignatoryAdmin)
+admin.site.register(smodels.Document, DocumentAdmin)
---
+++ django_digital_signature/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=55)),
+                ('date_created', models.DateTimeField(editable=False)),
+                ('object_pk', models.IntegerField(null=True, editable=False, blank=True)),
+                ('author', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('content_type', models.ForeignKey(blank=True, editable=False, to='contenttypes.ContentType', null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Signatory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=30)),
+                ('auth_token', models.CharField(max_length=255)),
+                ('date_signed', models.DateTimeField(editable=False)),
+                ('document', models.ForeignKey(to='django_digital_signature.Document')),
+                ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+        ),
+    ]
