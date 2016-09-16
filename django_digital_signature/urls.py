c4ecea968f3fe353aea06b31e00abb8583b145ec
=yeago <yeago999@gmail.com>
2016-09-15 18:14:08 -0700

    1.10

--- django_digital_signature/urls.py
+++ django_digital_signature/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from django_digital_signature.views import apply_signature
 
-urlpatterns = patterns('django_digital_signature.views',
-    url("^apply-signature/$", "apply_signature", name="apply_signature"),
-)
+urlpatterns = [
+    url("^apply-signature/$", apply_signature, name="apply_signature"),
+]
